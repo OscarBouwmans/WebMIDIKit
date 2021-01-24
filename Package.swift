ebff41eaa2fe878b3530fc38fabbe5c5103eb133
Oscar Bouwmans <oscarbouwmans@me.com>
2021-01-24 20:58:11 +0100

    Add iOS to Swift Package

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,7 @@ let package = Package(
 	
     name: "WebMIDIKit",
 	
-	platforms: [.macOS(.v10_11)],
+	platforms: [.macOS(.v10_11), .iOS(.v11)],
 	
 	products: [
         .library(
